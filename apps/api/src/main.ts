668d881616e054b54a04db53a5fc1d0fa1b76b3a
Pedro Oliveira <peoliveira0101@gmail.com>
2026-05-05 16:05:33 -0300

    chore(api): configure CORS, global prefix and env files

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    methods: ['GET'],
+  });
+
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
